af9950718c0d0aba46799d30142a98b61becdafb
casper22875 <casper22875@gmail.com>
2015-08-30 10:58:02 -0400

    we defined p1 and p2 markers

--- groupttt.rb
+++ groupttt.rb
@@ -8,3 +8,23 @@ end
 
 arena("Welcome to Tic Tac Toe")
 arena("This is the current board")
+
+def player1_marker
+	puts "Player 1 please choose X or O."
+	gets.chomp.upcase
+end
+
+player1 = player1_marker
+
+puts "Player 1 has chosen #{player1}."
+
+def player2_marker(marker1)
+if marker1 == "X"
+then "O"
+else "X"
+end
+end
+
+player2 = player2_marker(player1)
+puts "player 2 is #{player2}"
+
